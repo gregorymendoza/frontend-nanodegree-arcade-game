fbe5d514e075b8d65152a0c2d3842635caa7c96d
Gregory Mendoza <gregmendgreg@gmail.com>
2016-12-09 18:28:11 -0400

    feat: Add basic game functionality

--- js/app.js
+++ js/app.js
@@ -1,7 +1,13 @@
+// Global variables
+var pStartX = 202;
+var pStartY = 300;
+
 // Enemies our player must avoid
-var Enemy = function() {
+var Enemy = function(x, y) {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
+    this.x = x;
+    this.y = y;
 
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
@@ -14,6 +20,7 @@ Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
+    this.x = (this.x < 505) ? this.x += dt * 160 : this.x = -82;
 };
 
 // Draw the enemy on the screen, required method for game
@@ -24,13 +31,58 @@ Enemy.prototype.render = function() {
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
+var Player = function() {
+    this.x = pStartX;
+    this.y = pStartY;
+    this.sprite = 'images/char-boy.png';
+};
 
+// Update the enemy's position, required method for game
+Player.prototype.update = function() {
+
+};
+
+// Draw the player on the screen, required method for game
+Player.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
+
+Player.prototype.handleInput = function(keyDirection) {
+    if(keyDirection == 'up' && this.y > -28) {
+        this.y -= 82;
+    }
+    if (this.y < 54) {
+        this.restart();
+    }
+    if(keyDirection == 'down' && this.y < 382) {
+        this.y += 82;
+    }
+    if(keyDirection == 'left' && this.x > 0) {
+        this.x -= 101;
+    }
+    if(keyDirection == 'right' && this.x < 400) {
+        this.x += 101;
+    }
+};
+
+Player.prototype.restart = function() {
+    this.x = pStartX;
+    this.y = pStartY;
+};
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
+var allEnemies = [
+    new Enemy(-282, 142),
+    new Enemy(-82, 225),
+    new Enemy(-482, 62),
+    new Enemy(-882, 142),
+    new Enemy(-382, 225),
+    new Enemy(-182, 62)
+];
+
 // Place the player object in a variable called player
-
-
+var player = new Player(pStartX, pStartY);
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
